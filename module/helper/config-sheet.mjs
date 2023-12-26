96b8724f3d7c2f301713c747a7eaa8d678aab4dc
Inigo Surguy <inigo.surguy@67bricks.com>
2023-12-26 15:57:31 +0000

    Add a starting message describing setup

--- module/helper/config-sheet.mjs
+++ module/helper/config-sheet.mjs
@@ -105,3 +105,36 @@ export const configSheet = async () => {
     await game.settings.set('pbta', 'hideRollMode', true);
     await game.settings.set('pbta', 'hideUses', true);
 }
+
+Hooks.once('init', () => {
+    game.settings.register('under-hollow-hills-for-pbta', 'firstTime', {
+        name: "First Time Flag",
+        hint: "Used to track if this is the first time the module is loaded in a game.",
+        scope: "world",  // This makes the setting specific to each world
+        config: false,  // This specifies that the setting does not appear in the settings menu
+        type: Boolean,
+        default: false,  // Default value is false, indicating the message hasn't been shown yet
+    });
+});
+
+
+Hooks.once('ready', async () => {
+    if (game.settings.get('under-hollow-hills-for-pbta', 'firstTime') === false) {
+        console.info("Sending first time welcome message");
+        let chatData = {
+            user: game.user.id,
+            speaker: ChatMessage.getSpeaker(),
+            content: "<p><b>Roll up! Roll up! The Circus is here!</b></p>" +
+                "<p>Please create characters via the Actor tab, select an appropriate Playbook (you can see the options in <em>Compendium Packs | Playbooks</em>), " +
+                "then drag in the Obvious Plays and your Playbook-specific Plays from <em>Compendium Packs | Plays</em>, the Playbook-specific Trappings from " +
+                "<em>Compendium Packs | Trappings</em>, and the appropriate Consequences from <em>Compendium Packs | Consequences</em>.</p>" +
+                "<p>For each of your Plays that have an associated roll, edit the Play on your character sheet and choose a <em>Roll</em> - you should have three with 'Plus two', " +
+                "two with 'Zero', two with 'Minus one', and the remainder should be 'Plus one'. These are only your plays’ starting rolls; you’ll have many opportunities to change them.</p>" +
+                "<p>In the <em>Description</em> section of your character sheet, enter your Imagery (for fairies, this will be for Summer and for Winter; for mortals the names differ but " +
+                "the mechanism is the same). See your Playbook for suggested imagery.</p>" +
+                "<p>NPCs and the Playbill are created as Actors. They do not have attributes - you can add information about them as free text in their Descriptions.</p>"
+        };
+        ChatMessage.create(chatData, {});
+        await game.settings.set('under-hollow-hills-for-pbta', 'firstTime', true);
+    }
+});
